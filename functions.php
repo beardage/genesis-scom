2273ec38b0a8ad465982aa2ca9bb94a1cd3ce798
Kyle Beard <kylebeard@nuancedmedia.com>
2016-05-16 17:35:03 -0700

    moving nav and footer stuff to independent file

--- functions.php
+++ functions.php
@@ -19,42 +19,14 @@ function sample_viewport_meta_tag() {
 }
 
 // stylesheet / js imports
-add_action( 'wp_enqueue_scripts', 'allnighter_scripts_and_styles' );
-function allnighter_scripts_and_styles() {
+add_action( 'wp_enqueue_scripts', 'scom_scripts_and_styles' );
+function scom_scripts_and_styles() {
 	wp_enqueue_style( 'various-styles', get_stylesheet_directory_uri() . '/css/compiled-styles.css' );
 	wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.css' );
 	wp_enqueue_script('jquery');
 	wp_enqueue_script( 'various-scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array(), array('jquery'), true );
 }
 
-
-
 // remove auto p tags
 remove_filter( 'the_content', 'wpautop' );
 remove_filter( 'the_excerpt', 'wpautop' );
-
-// Add support for 3-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 1 );
-
-
-// no footer as of now
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-
-
-// set up navigation
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-remove_action('genesis_header', 'genesis_do_header');
-add_action('genesis_before', 'an_do_header');
-function an_do_header() {
-	?>
-	<div class="site-header">
-		<div class="logo left">
-			<a href="/" data-text="kylebeard //">kylebeard //</a>
-		</div>
-
-		<ul class="navigation right">
-			<?php genesis_do_nav();  ?>
-		</ul>
-	</div>
-	<?php
-}
---
+++ lib/structure/header-and-footer.php
@@ -0,0 +1,22 @@
+<?php
+
+// Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+// set up navigation
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+remove_action('genesis_header', 'genesis_do_header');
+add_action('genesis_before', 'an_do_header');
+function an_do_header() {
+	?>
+	<div class="site-header">
+		<div class="logo left">
+			<a href="/">SCOM</a>
+		</div>
+
+		<ul class="navigation right">
+			<?php genesis_do_nav();  ?>
+		</ul>
+	</div>
+	<?php
+}
